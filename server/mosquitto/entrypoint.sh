a343e5608ed23bde871a4c5bc8709a519d041e4f
naad <naad.229301420@muj.manipal.edu>
2026-04-28 00:12:39 +0530

    update entrypoint.sh to clear password file before creating new Mosquitto user

--- server/mosquitto/entrypoint.sh
+++ server/mosquitto/entrypoint.sh
@@ -2,8 +2,8 @@
 set -e
 
 if [ -n "$MQTT_USER" ] && [ -n "$MQTT_PASSWORD" ]; then
-  rm -f /mosquitto/config/passwd
-  mosquitto_passwd -b -c /mosquitto/config/passwd "$MQTT_USER" "$MQTT_PASSWORD"
+  : > /mosquitto/config/passwd
+  mosquitto_passwd -b /mosquitto/config/passwd "$MQTT_USER" "$MQTT_PASSWORD"
 fi
 
 exec "$@"
