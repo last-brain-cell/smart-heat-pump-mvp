2983897add39bdf93d2ab90d676c02077ae62a9e
naad <naad.229301420@muj.manipal.edu>
2026-04-27 19:42:36 +0530

    update Dockerfile to set permissions and create Mosquitto password file

--- server/mosquitto/entrypoint.sh
+++ server/mosquitto/entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 if [ -n "$MQTT_USER" ] && [ -n "$MQTT_PASSWORD" ]; then
+  rm -f /mosquitto/config/passwd
   mosquitto_passwd -b -c /mosquitto/config/passwd "$MQTT_USER" "$MQTT_PASSWORD"
 fi
 
